42606b108b4e3b017aa1630356158cd908ffa873
JRproyects <168209924+JRproyects@users.noreply.github.com>
2025-10-16 17:51:42 -0300

    Inicial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        aqua: '#00BFFF',
+        deepBlue: '#003366',
+        lightBlue: '#e0f7fa',
+      },
+    },
+  },
+  plugins: [],
+}
